6fdeca389e0017416cfd23ca9af51cc1aa05fc63
ansernawaz2012 <ansernawaz2012@gmail.com>
2019-03-21 10:23:17 +0000

    Updates

---
+++ precache-manifest.84909b2baaf77cc3fe24332266e3c674.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d721d06f78c96d55f57",
+    "url": "/calculator/static/js/runtime~main.19f1d22d.js"
+  },
+  {
+    "revision": "a3c6ad5d6b6f0d2c9e1b",
+    "url": "/calculator/static/js/main.6950d81e.chunk.js"
+  },
+  {
+    "revision": "c7f0d85f56bdeec1e63e",
+    "url": "/calculator/static/js/2.0824b821.chunk.js"
+  },
+  {
+    "revision": "a3c6ad5d6b6f0d2c9e1b",
+    "url": "/calculator/static/css/main.26642417.chunk.css"
+  },
+  {
+    "revision": "2346d1720034a0dd9b24c6adde9ad904",
+    "url": "/calculator/index.html"
+  }
+];
